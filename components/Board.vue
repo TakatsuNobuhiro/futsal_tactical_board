580d58b62a11fadbbda5596b61df96a053abfda2
高津亘宏 <takatsuwataruhiroshi@TPMB-29.local>
2021-08-10 15:56:01 +0900

    refactor 座標をdateに格納

--- components/Board.vue
+++ components/Board.vue
@@ -1,30 +1,18 @@
 <template>
     <div id="board">
         <VueDragResize  :aspectRatio="true" :isResizable="false" :isDraggable="false"  :preventActiveBehavior="true" >
-            <!-- <img src="/image/board.png" width="100%"> -->
-            <!-- red -->
 
-            <VueDragResize v-on:dragging="resize" :aspectRatio="true" :isResizable="false" :w="50" :h="50" :x="450" :y="480" >
-                <div  class="of circle object">1</div>
+            <!-- red -->
+            <VueDragResize v-for="red_object in red_objects" :key="red_object.index" v-on:dragging="resize" :aspectRatio="true" :isResizable="false" :w="50" :h="50" :x=red_object.x :y=red_object.y >
+                <div  class="of circle object">{{red_object.index}}</div>
             </VueDragResize>
-            <VueDragResize v-on:dragging="resize" :aspectRatio="true" :isResizable="false" :w="50" :h="50" :x="550" :y="150">
-                <div  class="of circle object">2</div>
-            </VueDragResize>
-            <VueDragResize v-on:dragging="resize" :aspectRatio="true" :isResizable="false" :w="50" :h="50" :x="550" :y="810">
-                <div  class="of circle object">3</div>
-            </VueDragResize>
-            <VueDragResize v-on:dragging="resize" :aspectRatio="true" :isResizable="false" :w="50" :h="50" :x="1300" :y="480">
-                <div  class="of circle object">4</div>
-            </VueDragResize>
-            <VueDragResize v-on:dragging="resize" :aspectRatio="true" :isResizable="false" :w="50" :h="50" :x="150" :y="480">
-                <div  class="of circle object">G</div>
-            </VueDragResize>
+
 
             <!-- blue -->
-            <VueDragResize v-on:dragging="resize" :aspectRatio="true" :isResizable="false" :w="50" :h="50" :x="600" :y="480">
-                <div class="df circle object">1</div>
+            <VueDragResize v-for="blue_object in blue_objects" :key="blue_object.index" v-on:dragging="resize" :aspectRatio="true" :isResizable="false" :w="50" :h="50" :x=blue_object.x :y=blue_object.y>
+                <div class="df circle object">{{blue_object.index}}</div>
             </VueDragResize>
-            <VueDragResize v-on:dragging="resize" :aspectRatio="true" :isResizable="false" :w="50" :h="50" :x="750" :y="230">
+            <!-- <VueDragResize v-on:dragging="resize" :aspectRatio="true" :isResizable="false" :w="50" :h="50" :x="750" :y="230">
                 <div class="df circle object">2</div>
             </VueDragResize>
             <VueDragResize v-on:dragging="resize" :aspectRatio="true" :isResizable="false" :w="50" :h="50" :x="750" :y="730">
@@ -35,12 +23,11 @@
             </VueDragResize>
             <VueDragResize v-on:dragging="resize" :aspectRatio="true" :isResizable="false" :w="50" :h="50" :x="1700" :y="480"> 
                 <div class="df circle object">G</div>
-            </VueDragResize>
+            </VueDragResize> -->
 
             <!-- ball -->
-            <VueDragResize v-on:dragging="resize" :aspectRatio="true" :isResizable="false" :w="30" :h="30" :x="500" :y="510"> 
+            <VueDragResize v-on:dragging="resize" :aspectRatio="true" :isResizable="false" :w="30" :h="30" :x=ball.x :y=ball.y> 
                 <font-awesome-icon :icon="['fas', 'futbol']" size="2x" class="object" />
-
             </VueDragResize>
             
         </VueDragResize>
@@ -59,6 +46,70 @@
     
 
     export default {
+        data(){
+            return{
+                red_objects:{
+                    red_1:{
+                        index:"1",
+                        x:450,
+                        y:480
+                    },
+                    red_2:{
+                        index:"2",
+                        x:550,
+                        y:150
+                    },
+                    red_3:{
+                        index:"3",
+                        x:550,
+                        y:810
+                    },
+                    red_4:{
+                        index:"4",
+                        x:1300,
+                        y:480
+                    },
+                    red_g:{
+                        index:"G",
+                        x:150,
+                        y:480
+                    }
+                },
+                blue_objects:{
+                    blue_1:{
+                        index:"1",
+                        x:600,
+                        y:480
+                    },
+                    blue_2:{
+                        index:"2",
+                        x:750,
+                        y:230
+                    },
+                    blue_3:{
+                        index:"3",
+                        x:750,
+                        y:730
+                    },
+                    blue_4:{
+                        index:"4",
+                        x:1400,
+                        y:480
+                    },
+                    blue_g:{
+                        index:"G",
+                        x:1700,
+                        y:480
+                    },
+                },
+                ball:{
+                    x:500,
+                    y:510
+                }
+            
+            }
+            
+        },
             
         computed: {
             red_width(){
@@ -94,7 +145,6 @@
         text-align:center;
         font-weight:bold;
         border-radius:50%;
-
     }
 
     .object{
